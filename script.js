e41c66b669b82842d94ff8f67716632b86b4ffc8
nad-15 <123393892+nad-15@users.noreply.github.com>
2025-02-23 22:47:32 +0800

    operators and numbers logic

--- script.js
+++ script.js
@@ -1,44 +1,67 @@
-const zero = document.getElementById('zero');
-const one = document.getElementById('one');
-const two = document.getElementById('two');
-const three = document.getElementById('three');
-const four = document.getElementById('four');
-const five = document.getElementById('five');
-const six = document.getElementById('six');
-const seven = document.getElementById('seven');
-const eight = document.getElementById('eight');
-const nine = document.getElementById('nine');
+// const zero = document.getElementById('zero');
+// const one = document.getElementById('one');
+// const two = document.getElementById('two');
+// const three = document.getElementById('three');
+// const four = document.getElementById('four');
+// const five = document.getElementById('five');
+// const six = document.getElementById('six');
+// const seven = document.getElementById('seven');
+// const eight = document.getElementById('eight');
+// const nine = document.getElementById('nine');
 
-const addButton = document.getElementById('addButton');
-const minus = document.getElementById('minus'); // Fixed ID
-const multiply = document.getElementById('multiply');
-const divideButton = document.getElementById('divideButton');
-const percentButton = document.getElementById('percentButton');
+// const addButton = document.getElementById('addButton');
+// const minus = document.getElementById('minus'); // Fixed ID
+// const multiply = document.getElementById('multiply');
+// const divideButton = document.getElementById('divideButton');
+// const percentButton = document.getElementById('percentButton');
+
+
+// const negateButton = document.getElementById('negateButton');
+// const decimalPoint = document.getElementById('decimal-point');
+// const equals = document.getElementById('equals');
 
 const clearButton = document.getElementById('clearButton');
 const backspaceButton = document.getElementById('backspaceButton');
-const negateButton = document.getElementById('negateButton');
-const decimalPoint = document.getElementById('decimal-point');
-const equals = document.getElementById('equals');
-
 const numbers = document.querySelectorAll('.numbers');
-
+const operators = document.querySelectorAll(`.operators`);
 const expressionDisplay = document.querySelector('.expression-dis');
-const resultDisplay = document.querySelector('.result-dis'); 
+const resultDisplay = document.querySelector('.result-dis');
+
+let isOperator = false;
+let isDecimal = false;
+let expressionArr = [];
 
 
-clearButton.addEventListener(`click`, ()=> {
+clearButton.addEventListener(`click`, () => {
+    expressionArr.length = 0;
     expressionDisplay.textContent = '0';
 });
 
-const operators = document.querySelectorAll(`.operators`);
+backspaceButton.addEventListener(`click`, ()=>{
+    expressionArr.pop();
+    expressionDisplay.textContent = expressionArr.join(``);
+
+})
+
 
 operators.forEach(operator => {
-    operator.addEventListener(`click`, performOperation);
+    operator.addEventListener(`click`, (e) => {
+        const operator = e.target.textContent;
+        if (!isOperator) {
+            expressionArr.push(operator);
+            expressionDisplay.textContent = expressionArr.join(``);
+            isOperator = true;
+        } else {
+            console.log(expressionArr);
+            expressionArr.pop();
+            expressionArr.push(operator);
+            expressionDisplay.textContent = expressionArr.join(``);
+        }
+    });
 });
 
-function performOperation () {
-
+function performOperation() {
+    //resultDisplay here
 }
 
 
@@ -47,15 +70,20 @@ numbers.forEach(number => {
 });
 
 function getNumber(e) {
+    isOperator = false;
     const number = e.target.textContent;
     console.log(number);
-    if (number !== `0`) {
-        //do numbers here except 0
-        // expressionDisplay.textContent+= number;
-        expressionDisplay.textContent = expressionDisplay.textContent === '0' ? number : expressionDisplay.textContent + number;
+    if (number === `0`) {
+        // do zero logic here
+
+    } else if (number === `.`){
+        //decimal logic here
+        
+        isDecimal = true;
 
     } else {
-        // do zero logic here
+        expressionArr.push(number);
+        expressionDisplay.textContent = expressionArr.join('');
     }
 }
 
